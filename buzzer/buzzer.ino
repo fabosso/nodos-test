de5b66610811ed3e07d95737a0b142564fb3d5c9
Franco Abosso <franco.abosso@gm2dev.com>
2022-09-18 14:23:34 -0300

    add buzzer test program

--- buzzer/buzzer.ino
+++ buzzer/buzzer.ino
@@ -1,29 +1,38 @@
-/// Dependencias de este programa:
+/// Dependencias de este programa: ninguna.
 
 // Declaración de defines.
 
-
+#define BUZZER_PIN A1
+#define BUZZER_ACTIVO HIGH
+#define BUZZER_INACTIVO LOW
 
 // Declaración de variables.
-
-
-
 
 ///////////////////////////////////////////////////////////////
 
 // Setup del arduino.
 
 void setup() {
-
+    #ifdef BUZZER_PIN
+        pinMode(BUZZER_PIN, OUTPUT);
+    #endif
 }
 
 // Rutina del arduino.
 
 void loop() {
-
+    alertMock();
 }
 
 //////////////////////////////////////////////////////////////
 
 // Zona de declaración de funciones.
+void alertMock() {
+    static unsigned long previousMillis = 0;
+    unsigned long currentMillis = millis();
 
+    if (currentMillis - previousMillis >= 500) {
+        digitalWrite(BUZZER_PIN, !digitalRead(BUZZER_PIN));
+        previousMillis = currentMillis;
+    }
+}
