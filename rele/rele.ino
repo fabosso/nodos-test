20b49f39d56aec8603d46dd95fbff3d37b45f2a6
Franco Abosso <franco.abosso@gm2dev.com>
2022-09-18 14:24:23 -0300

    add relay test program

--- rele/rele.ino
+++ rele/rele.ino
@@ -2,25 +2,33 @@
 
 // Declaración de defines.
 
+#define RELE_PIN 7
+#define RELE_ACTIVO HIGH        // definido según jumper de módulo
+#define RELE_INACTIVO LOW       // definido según jumper de módulo
 
 
 // Declaración de variables.
-
-
-
 
 ///////////////////////////////////////////////////////////////
 
 // Setup del arduino.
 
 void setup() {
-
+    #ifdef RELE_PIN
+        pinMode(RELE_PIN, OUTPUT);
+    #endif
 }
 
 // Rutina del arduino.
 
 void loop() {
+    static unsigned long previousMillis = 0;
+    unsigned long currentMillis = millis();
 
+    if (currentMillis - previousMillis >= 1000) {
+        digitalWrite(RELE_PIN, !digitalRead(RELE_PIN));
+        previousMillis = currentMillis;
+    }
 }
 
 //////////////////////////////////////////////////////////////
