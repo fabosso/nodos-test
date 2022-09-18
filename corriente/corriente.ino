70ab83e7e871ae35c12e597a6851a079ab2a5d23
Franco Abosso <franco.abosso@gm2dev.com>
2022-09-18 14:23:43 -0300

    add corriente test program

--- corriente/corriente.ino
+++ corriente/corriente.ino
@@ -1,29 +1,48 @@
 /// Dependencias de este programa:
+// Energy Monitor:
+#include <EmonLib.h>            // https://learn.openenergymonitor.org/electricity-monitoring/ctac/how-to-build-an-arduino-energy-monitor
 
 // Declaración de defines.
 
+#define CORRIENTE_PIN A1
+#define EMON_CALIBRATION 111.1
+#define EMON_CROSSINGS 20 // Cantidad de semi-ondas muestreadas para medir tensión y/o corriente.
+#define EMON_TIMEOUT 1000 // Timeout de la rutina calcVI (en ms).
 
 
 // Declaración de variables.
 
-
-
+EnergyMonitor eMon;
 
 ///////////////////////////////////////////////////////////////
 
 // Setup del arduino.
 
 void setup() {
-
+    Serial.begin(9600);
+    #ifdef CORRIENTE_PIN
+        eMon.current(CORRIENTE_PIN, EMON_CALIBRATION); 
+    #endif
 }
 
 // Rutina del arduino.
 
 void loop() {
+    static unsigned long previousMillis = 0;
+    unsigned long currentMillis = millis();
 
+    if (currentMillis - previousMillis >= 2000) {
+        eMon.calcVI(EMON_CROSSINGS, EMON_TIMEOUT);
+        getNewCurrent();
+        previousMillis = currentMillis;
+    }
 }
 
 //////////////////////////////////////////////////////////////
 
 // Zona de declaración de funciones.
-
+void getNewCurrent() {
+    Serial.print("Nueva corriente: ");
+    Serial.print(eMon.Irms);
+    Serial.println(" A");
+}
